6a4e8bdca797ef58b7972434cce8ac7e7663d41a
musaibahmedrazzaqui <104699056+musaibahmedrazzaqui@users.noreply.github.com>
2022-09-22 15:56:56 +0500

    updated UI of Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,20 +11,21 @@ const Home = () => {
       <div className="text_container">
         <h1>These are the following steps:</h1>
         <br />
+
+        <div className="button_div">
+          <Link to="/map">
+            <Button variant="contained">Begin!</Button>
+          </Link>
+        </div>
         <h3>
           1. Click the begin button <br /> 2. Tap Allow on the prompt of
           permission to use your location - you're redirected to your location{" "}
-          <br /> 3. Click the Next Step button <br /> 4. Add your name and
-          nearest landmark according to your location and press submit{" "}
+          <br /> 3. Make sure your location is turned on your device <br /> 4.
+          Click the Next Step button <br /> 4. Add your name and nearest
+          landmark according to your location and press submit{" "}
         </h3>
         <br />
-        <br />
-        <h4> We thank you for helping us in our project. </h4>
-      </div>
-      <div className="button_div">
-        <Link to="/map">
-          <Button variant="contained">Begin!</Button>
-        </Link>
+        <h5> We thank you for helping us in our project. </h5>
       </div>
     </div>
   );
